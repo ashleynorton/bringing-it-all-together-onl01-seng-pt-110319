0e3ccfaf64a8ceeb3c403de9f5f8d2de7f581652
Ashley Norton <nortonashleym@gmail.com>
2020-02-24 17:07:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,18 @@ class Dog
     new_dog
   end 
 
-
-
+  def find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1 
+    SQL
+    
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end 
 
 
 
