5e89c7b4e7c1f20c7fdf8bd5c3743fc41871b4ab
Ashley Norton <nortonashleym@gmail.com>
2020-02-24 17:12:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,9 +67,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     binding.pry
-    if !dog.empty?
-      dog_info = dog[0]
-      dog = Dog.new()
+  
   end 
     
   def self.find_by_name(name)
