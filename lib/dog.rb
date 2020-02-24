486be207793fb10889ead3c8b1f03a2a494c9fdb
Ashley Norton <nortonashleym@gmail.com>
2020-02-24 17:33:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,17 +65,15 @@ class Dog
   end 
     
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed).first 
    
     if dog
-      dog_info = self.new_from_db(dog)
+      dog = self.new_from_db(dog)
     else 
-      dog = Dog.create(name: name, breed: breed)
+      dog = self.create(name: name, breed: breed)
     end 
     
-    if dog.name
-      
-    dog 
+    dog
   end 
     
   def self.find_by_name(name)
